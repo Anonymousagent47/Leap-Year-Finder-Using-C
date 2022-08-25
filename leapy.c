90c32c3fbd39ae6ea0ff4952d61fc00b2c77f2cc
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-08-25 20:46:37 +0530

    Add files via upload
    This Code is for Finding Year is leap or not .....
    Hope it will be helpful..
    Enjoy!!

---
+++ leapy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter Any Year (4 Digit): ");
+    scanf("%d",&year);
+        if(year % 4 == 0 ){
+            printf("This is Leap Year");
+        }else{
+            printf("This is not Leap Year");
+        }
+}
