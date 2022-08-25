691354ea3382c3fb6c252b335230c8b7bd28b509
Prince Solanki <87561928+Anonymousagent47@users.noreply.github.com>
2022-08-25 20:48:05 +0530

    Update leapy.c

--- leapy.c
+++ leapy.c
@@ -4,7 +4,7 @@ int main()
     int year;
     printf("Enter Any Year (4 Digit): ");
     scanf("%d",&year);
-        if(year % 4 == 0 ){
+        if(year % 4 == 0 || year % 400 == 0){
             printf("This is Leap Year");
         }else{
             printf("This is not Leap Year");
